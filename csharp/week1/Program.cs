2ee1e55d00e40113da4de8ca13f49a0282d0ecd2
GQi <1521545037@qq.com>
2018-09-07 11:40:01 +0800

    2.3变量

--- csharp/week1/Program.cs
+++ csharp/week1/Program.cs
@@ -6,17 +6,36 @@ using System.Threading.Tasks;
 
 namespace week1
 {
-    #region 第一个C#程序
-    public class Program1
+    #region 2.2第一个C#程序
+    //public class Program1
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Hello from Wrox.");
+    //        Console.ReadLine();
+    //        return;
+    //    }
+    //}
+    #endregion
+
+    #region 2.3变量
+    public class Program2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello from Wrox.");
-            Console.ReadLine();
-            return;
+            var name = "Bugs Bunny";
+            var age = 25;
+            var isRabbit = true;
+
+            Type nameType = name.GetType();
+            Type ageType = age.GetType();
+            Type isRabbitType = isRabbit.GetType();
+
+            Console.WriteLine("name is type " + nameType.ToString());
+            Console.WriteLine("age is type " + ageType.ToString());
+            Console.WriteLine("isRabbit is type " + isRabbitType.ToString());
+            Console.ReadLine();s
         }
     }
     #endregion
-
-
 }
