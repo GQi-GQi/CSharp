57614e8e9a6c7ed859e90d750c911db29030b313
GQi <1521545037@qq.com>
2018-09-07 12:00:30 +0800

    2.3.2变量作用域

--- csharp/week1/Program.cs
+++ csharp/week1/Program.cs
@@ -19,22 +19,40 @@ namespace week1
     #endregion
 
     #region 2.3变量
-    public class Program2
+    //public class Program2
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        var name = "Bugs Bunny";
+    //        var age = 25;
+    //        var isRabbit = true;
+
+    //        Type nameType = name.GetType();
+    //        Type ageType = age.GetType();
+    //        Type isRabbitType = isRabbit.GetType();
+
+    //        Console.WriteLine("name is type " + nameType.ToString());
+    //        Console.WriteLine("age is type " + ageType.ToString());
+    //        Console.WriteLine("isRabbit is type " + isRabbitType.ToString());
+    //        Console.ReadLine();s
+    //    }
+    //}
+    #endregion
+
+    #region 2.3.2变量作用域
+    public class  Program3
     {
-        static void Main(string[] args)
+        public static void Main(string[]  args)
         {
-            var name = "Bugs Bunny";
-            var age = 25;
-            var isRabbit = true;
-
-            Type nameType = name.GetType();
-            Type ageType = age.GetType();
-            Type isRabbitType = isRabbit.GetType();
-
-            Console.WriteLine("name is type " + nameType.ToString());
-            Console.WriteLine("age is type " + ageType.ToString());
-            Console.WriteLine("isRabbit is type " + isRabbitType.ToString());
-            Console.ReadLine();s
+            for (int i = 0; i < 10; i++)     //i只作用于这个循环
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 9; i >= 0; i--)     //i只作用于这个循环
+            {
+                Console.WriteLine(i);
+            }
+            return;
         }
     }
     #endregion
