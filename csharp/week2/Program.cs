dba1e06f36164efb1d454703703bac999b409828
GQi <1521545037@qq.com>
2018-09-12 00:41:19 +0800

    2.10 控制台

--- csharp/week2/Program.cs
+++ csharp/week2/Program.cs
@@ -7,15 +7,24 @@ using System.Threading.Tasks;
 
 namespace program
 {
-    class ArgsExample           //使用命令就可以编译这段代码。在运行编译好的可执行文件时，可以在程序名的后面加上参数。eg：ArgeExample /a/b/c
+    class MathAdd       //控制台
     {
-        public static int Main(string[] args)
+        static void Main()
         {
-            for (int i = 0; i < args.Length; i++)
-            {
-                Console.WriteLine(args[i]);
-            }
-            return 0;
+            int i = 10;
+            int j = 20;
+            Console.WriteLine("{0} plus {1} equals {2}", i, j, i + j);
+
+            Console.WriteLine(" {0,4}\n+{1,4}\n--------\n {2,4}", i, j, i + j);
+            decimal k = 940.23m;
+            decimal l = 73.7m;
+            Console.WriteLine(" {0,9:C2}\n+{1,9:C2}\n-----------\n {2,9:C2}", k, l, k + l);
+
+            double d = 11.004;
+            Console.WriteLine("{0:#.00}", d);
+
+            Console.ReadLine();
+            return;
         }
     }
 }
