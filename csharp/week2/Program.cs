c574767c41af9ec962365a202c3ee20555e253e0
GQi <1521545037@qq.com>
2018-09-12 12:01:06 +0800

    3.3.2 函数成员--方法

--- csharp/week2/Program.cs
+++ csharp/week2/Program.cs
@@ -5,26 +5,41 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace program
+namespace Program
 {
-    class MathAdd       //控制台
+    class Program      //方法
+    {
+        //方法的声明
+        public bool IsPositive(int value)
+        {
+            if (value < 0)
+                return false;
+            return true;
+        }
+    }
+    class MainEntryOoint
     {
         static void Main()
         {
-            int i = 10;
-            int j = 20;
-            Console.WriteLine("{0} plus {1} equals {2}", i, j, i + j);
+            Console.WriteLine("Pi is " + MathTest.GetPi());
+        }
+    }
+    class MathTest
+    {
+        public int value;
+        public int GetSquare()
+        {
+            return value * value;
+        }
 
-            Console.WriteLine(" {0,4}\n+{1,4}\n--------\n {2,4}", i, j, i + j);
-            decimal k = 940.23m;
-            decimal l = 73.7m;
-            Console.WriteLine(" {0,9:C2}\n+{1,9:C2}\n-----------\n {2,9:C2}", k, l, k + l);
+        public static int GetSquareOf(int x)
+        {
+            return x * x;
+        }
 
-            double d = 11.004;
-            Console.WriteLine("{0:#.00}", d);
-
-            Console.ReadLine();
-            return;
+        public static double GetPi()
+        {
+            return 3.1415926;
         }
     }
 }
