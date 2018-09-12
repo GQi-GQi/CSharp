80cb926cb0bb352cbf3de072089dd96c59c5740c
GQi <1521545037@qq.com>
2018-09-12 18:02:57 +0800

    3.6 弱引用

--- csharp/week2/Program.cs
+++ csharp/week2/Program.cs
@@ -5,12 +5,30 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace Program               //3.4 匿名类型
+namespace Program               //3.6 弱引用
 {
     public class MyClass     //创建一个类
     {
-
-        var captain = new { FirstName = "James", MiddleName = "T", LastName = "Kirk" };
+        static void Main()
+        {
+            WeakReference mathReference = new WeakReference(new MathTest());        //弱引用WeakReference使用
+            MathTest math;
+            if(mathReference.IsAlive)
+            {
+                math = mathReference.Target as MathTest;
+                math.Value = 30;
+            }
+            else
+            {
+                Console.WriteLine("Reference is not available.");
+            }
+            return;
+        }
+        class MathTest
+        {
+            public int Value;
+        }
+        
     }
     
 }
