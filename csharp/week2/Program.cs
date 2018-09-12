1d22de944309a69e03a72ba28fbea13a672da026
GQi <1521545037@qq.com>
2018-09-12 16:40:02 +0800

    3.3.3 只读字段

--- csharp/week2/Program.cs
+++ csharp/week2/Program.cs
@@ -5,22 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace Program               //3.3.2.3 构造函数（构造函数调用其他构造函数）
+namespace Program               //3.3.3 只读字段
 {
-    public class Car     //创建一个类
+    public class DocumentEditor     //创建一个类
     {
-        private string description;
-        private uint nWheels;
-        public Car(string description,uint nWheel)
-        {
-            this.description = description;
-            this.nWheels = nWheels;
-        }
+        public static readonly uint MaxDocunebts;
 
-        public Car(string description)
+        static DocumentEditor()
         {
-            this.description = description;
-            this.nWheels = 4;
+            MaxDocunebts = DoSomethingToFindOutMaxNumber();//未定义该方法
         }
     }
     
