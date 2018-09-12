aa806597a4da798a465f1bbde1987b7d1d90eb66
GQi <1521545037@qq.com>
2018-09-12 18:15:43 +0800

    3.5 结构

--- csharp/week2/Program.cs
+++ csharp/week2/Program.cs
@@ -5,29 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace Program               //3.6 弱引用
+namespace Program               //3.5 结构
 {
+    struct Dimensions       //创建一个结构类型
+    {
+        public double Length;
+        public double Width;
+
+        public Dimensions(double length,double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public double Diagonal
+        {
+            get
+            {
+                return Math.Sqrt(Length * Length + Width * Width);
+            }
+        }
+    }
     public class MyClass     //创建一个类
     {
-        static void Main()
-        {
-            WeakReference mathReference = new WeakReference(new MathTest());        //弱引用WeakReference使用
-            MathTest math;
-            if(mathReference.IsAlive)
-            {
-                math = mathReference.Target as MathTest;
-                math.Value = 30;
-            }
-            else
-            {
-                Console.WriteLine("Reference is not available.");
-            }
-            return;
-        }
-        class MathTest
-        {
-            public int Value;
-        }
         
     }
     
