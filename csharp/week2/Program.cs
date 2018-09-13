66a0986e0b37627aa6fbae26d75446ff93f4ff38
GQi <1521545037@qq.com>
2018-09-13 14:07:17 +0800

    4.3.5 密封类和密封方法

--- csharp/week2/Program.cs
+++ csharp/week2/Program.cs
@@ -4,8 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-abstract class CustomerAccount        //创建一个抽象类
+sealed class CustomerAccount        //创建一个密封类（不能继承该类）
 {
-    public abstract decimal CalcuateHeatingCost();      //定义一个抽象函数
+    public sealed override void FinalMethod()
+    {
+
+    }
+}
+
+class DerivedClass: CustomerAccount     //不能派生密封类
+{
+    public sealed override void FinalMethod()   //不能重写密封方法S
+    {
+
+    }
 }
