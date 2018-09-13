8a456c2fa2d891f1d28aa3d18258a82a7a14d95b
GQi <1521545037@qq.com>
2018-09-13 20:33:34 +0800

    4.3.6派生类的构造函数

--- csharp/week2/Program.cs
+++ csharp/week2/Program.cs
@@ -4,18 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-sealed class CustomerAccount        //创建一个密封类（不能继承该类）
+public abstract class  GenericCustomer     //创建一个抽象的类
 {
-    public sealed override void FinalMethod()
+    private string name;
+    public GenericCustomer(string name)
     {
+        this.name = name;
+    }
 
+    
+}
+
+class Nevermore60Customer: GenericCustomer      //派生出来一个类
+{
+    private uint highCostMinutesUsed;
+    public string referrerName;
+    public Nevermore60Customer(string name,string referrerName)     
+        : base(name)
+    {
+        this.referrerName = referrerName;
+    }
+    public Nevermore60Customer(string Name)
+        : this(Name,"<None>")
+    {
     }
 }
 
-class DerivedClass: CustomerAccount     //不能派生密封类
+class program2
 {
-    public sealed override void FinalMethod()   //不能重写密封方法S
+    static void Main(string[] args)
     {
-
+        Nevermore60Customer customer = new Nevermore60Customer("luoyaohui","laijunfu");
+        Console.WriteLine("Name is " + customer.name + ",referrerName is" + customer.referrerName);
+        return;
     }
 }
